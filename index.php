eba1eb814d456a26636e24055012b13fd839ca1a
illaweb35 <illaweb35@gmail.com>
2018-05-28 11:32:00 +0200

    index d'entrée sur le site

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+use Alaska\App\Router;
+
+require_once('../App/Autoloader.php');
+require_once('../App\Config.php');
+Autoloader::init();
+$route = new Router();
+$route->initRoute();
