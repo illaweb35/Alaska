cad42100e725050a5acd22480fd26a4726f1e122
illaweb35 <illaweb35@gmail.com>
2018-05-28 11:42:53 +0200

    Autoloader et singleton

---
+++ App/Autoloader.php
@@ -0,0 +1,18 @@
+<?php
+use Alaska\App\Pattern\Singleton;
+
+class Autoloader
+{
+    use Singleton;
+
+    public static function init()
+    {
+        spl_autoload_register([__CLASS__,'autoload']);
+    }
+    public static function autoload($class)
+    {
+        $class = str_replace(__NAMESPACE__, '', $class);
+        $class = str_replace('\\', '/', $class);
+        require_once(dirname(__DIR__).'/'.$class.'.php');
+    }
+}
---
+++ App/Pattern/Singleton.trait.php
@@ -0,0 +1,18 @@
+<?php
+namespace Alaska\App\Pattern;
+
+trait Singleton
+{
+    protected static $_instance = null;
+
+    private function __construct()
+    {
+    }
+    public static function getInstance()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new Singleton();
+        }
+        return self::$_instance;
+    }
+}
