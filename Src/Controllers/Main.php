53052b1a9e0efbca240b2b9010e60063fb774d97
illaweb35 <illaweb35@gmail.com>
2018-05-28 12:38:07 +0200

    Main Controller

---
+++ Src/Controllers/Main.php
@@ -0,0 +1,20 @@
+<?php
+namespace Src\Controllers;
+
+use Src\Managers\Billets;
+use Src\Managers\Comments;
+
+class Main
+{
+    private $_id;
+
+    public function __construct()
+    {
+        if (\session_status() == PHP_SESSION_NONE) {
+            \session_start();
+        }
+        $this->_id = (int)(!empty($_GET['id']) ? $_GET[id]: 0);
+        $this->Billets = new Billets();
+        $this->Comments = new Comments();
+    }
+}
