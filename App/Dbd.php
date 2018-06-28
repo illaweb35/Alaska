fe19a233006b63f1087fea8c5ff5fc9e1017460a
illaweb35 <illaweb35@gmail.com>
2018-06-28 10:15:13 +0200

    try catch sur Dbd

--- App/Dbd.php
+++ App/Dbd.php
@@ -21,9 +21,12 @@ class Dbd extends \PDO
         $db_user = \DB_USER;
         $db_pass =\DB_PASS;
         $dsn = ('mysql:host='.$db_host.';dbname='.$db_base.';charset=utf8');
-
-        parent::__construct($dsn, $db_user, $db_pass);
-        $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $this->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        try {
+            parent::__construct($dsn, $db_user, $db_pass);
+            $this->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            $this->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            throw new \Exception($e->getMessage(), 1);
+        }
     }
 }
